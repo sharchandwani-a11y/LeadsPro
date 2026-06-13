0e35a0fa565f616f256c03700140dce90cbd6774
Sneha Harchandwani <sharchandwani@gmail.com>
2026-06-13 18:23:24 +0530

    Remove DB fallback values

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 // Connection Configuration
 const dbConfig = {
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'leadspro',
+ host: process.env.DB_HOST,
+user: process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME,
   port: 3306
 };
 
